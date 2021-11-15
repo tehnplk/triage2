e07d4397da1eea8d65d55b841529fef3b192fe7d
tehnplk <tehnplk@gmail.com>
2021-11-15 14:51:52 +0000

    tehnn

--- views/site/index.php
+++ views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'PLK Triage Center';
         <p class="lead">ระบบสารสนเทศเพื่อการคัดแยกและส่งต่อผู้ป่วย COVID-19</p>
 
         <p>สำนักงานสาธารณสุขจังหวัดพิษณุโลก</p>
-        <div>
+        <div style="font-size: 28px">
             <span style="color:blue"><i class="fas fa-circle"></i></span> <span style="color:limegreen"><i class="fas fa-circle"></i></span> <span style="color:yellow"><i class="fas fa-circle"></i></span> <span style="color:#ff4500"><i class="fas fa-circle"></i></span>
         </div>
     </div>
