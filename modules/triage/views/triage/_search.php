2ddc8348eab3aa84bcb17ff26d89685e0c581bed
tehnplk <tehnplk@gmail.com>
2021-11-17 08:35:11 +0000

    tehnn

--- modules/triage/views/triage/_search.php
+++ modules/triage/views/triage/_search.php
@@ -21,13 +21,6 @@ use app\components\MyRole;
 
 
     <div class="row">
-
-        <div class="col-2" style="display: <?= MyRole::can_tri() ? '' : 'none' ?>">
-
-            <button type="button" class="btn btn-primary btn-auto"><i class="far fa-check-circle"></i> จัดกลุ่มอัตโนมัติ</button>
-
-        </div>
-
         <div class="col-3">
             <?php //echo $form->field($searchModel, 'triage_date') ?>
             <?php
@@ -47,7 +40,11 @@ use app\components\MyRole;
             ?>
         </div>
 
+        <div class="col" style="display: <?= MyRole::can_tri() ? '' : 'none' ?>">
 
+            <button type="button" class="btn btn-primary btn-auto"><i class="far fa-check-circle"></i> จัดกลุ่มผู้ป่วยอัตโนมัติ</button>
+
+        </div>
     </div>
 
 
