3f56b7e2ca2fb878d880cb1b2ac223e043b6d79b
tehnplk <tehnplk@gmail.com>
2021-11-18 05:51:07 +0000

    tehnn

---
+++ modules/triage/controllers/ExportController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\modules\triage\controllers;
+
+use yii\web\Controller;
+use app\components\MyTriage;
+
+/**
+ * Default controller for the `triage` module
+ */
+class AjaxController extends Controller {
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionAutoTriage($triage_date = NULL) {
+
+        if (empty($triage_date)) {
+            $date = date('Y-m-d');
+        } else {
+            $date = $triage_date;
+        }
+        $sql = "select t.id from triage t where (t.color is null or trim(t.color)='' or t.color='ฟ้า') and t.triage_date = '$date' ";
+        $raw = \Yii::$app->db->createCommand($sql)->queryAll();
+        $n = 0;
+        foreach ($raw as $p) {
+            $triage_id = $p['id'];
+            MyTriage::triage($triage_id);
+            $n++;
+        }
+        return "จัดกลุ่มทั้งหมด $n คน เรียบร้อยแล้ว!!!  ($date)";
+    }
+
+}
